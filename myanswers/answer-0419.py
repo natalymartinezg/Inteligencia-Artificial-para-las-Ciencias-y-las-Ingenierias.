f8fdbb028f2cc35d4d525a949f43d2a65ab7dbcb
natalymartinezg <nataly.martinezg@udea.edu.co>
2026-05-14 09:09:18 -0500

    Update answer-0419.py

--- myanswers/answer-0419.py
+++ myanswers/answer-0419.py
@@ -7,14 +7,23 @@ def proyectar_y_reconstruir_pca(X, n_componentes):
     scaler = StandardScaler()
     X_escalada = scaler.fit_transform(X)
 
-    pca = PCA(n_components=n_componentes, svd_solver='full')
+    pca = PCA(
+        n_components=n_componentes,
+        svd_solver='full'
+    )
 
     X_proyectada = pca.fit_transform(X_escalada)
 
     X_reconstruida_escalada = pca.inverse_transform(X_proyectada)
 
-    X_reconstruida = scaler.inverse_transform(X_reconstruida_escalada)
+    X_reconstruida = scaler.inverse_transform(
+        X_reconstruida_escalada
+    )
 
     varianza_explicada = pca.explained_variance_ratio_
 
-    return (X_proyectada, X_reconstruida, varianza_explicada)
+    return (
+        X_proyectada,
+        X_reconstruida,
+        varianza_explicada
+    )
